5a4ac08454edf15ecb68f69366c1c39ee3e85c79
Mandakini Agarwal <Mandakini@us.brainscale.com>
2021-06-12 00:22:23 +0530

    Add project files.

---
+++ PromotionsEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionsEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
